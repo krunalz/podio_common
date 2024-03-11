142f23d15e4aeb85c1866d05e2d9ee470dee0a86
krunalz <harsh.vekariya@m2webdesigning.com>
2024-03-11 14:27:24 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='podio-common',
+    name='podio_common',
     version='1.0.1',
     packages=find_packages(),
     install_requires=[
